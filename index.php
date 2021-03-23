1156b7aac87bf468a114b9fb581936af9d271f97
Aldo da Silva Barbosa <77800438+oaldobarbosa@users.noreply.github.com>
2021-03-23 08:54:27 -0300

    direcionamento para login

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
 <?php 
-include_once "./app/conexao/Conexao.php";
-
-require_once "app/login/login.php";
- ?>
-
-
-
-
-<!--
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Web Hotelao</title>
-</head>
-<body>
-
-	<div id="menu">
-		<nav>
-			<a href="./app/view/hospedar.php">Hospedar</a>
-			<a href="./app/view/hospedes.php">Hóspedes</a>
-			<a href="./app/view/hospedagem.php">Hospedagens</a>
-			<a href="./app/view/quartos.php">Quarto</a>
-			<a href="./app/view/checkout.php">Checkout</a>
-		</nav>
-		
-	</div>
-	<h1>Página Inicial</h1>
-
-</body>
-</html>
--->
+	header("Location: ../webhotelao/app/login/login.php");
+?>
